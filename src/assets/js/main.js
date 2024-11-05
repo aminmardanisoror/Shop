6a39975548d0cc5c7f4fc99b3bfcefa2ddb7f00d
amin mardani <amin.mardanisoror.sf@gmail.com>
2024-11-05 16:19:58 +0330

    fourth

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,7 @@
+let allPosts = [];
+let allComments = [];
 let reqComments;
+
 const reqPosts = new XMLHttpRequest();
 
 reqPosts.open("GET", "https://jsonplaceholder.typicode.com/posts");
@@ -13,28 +16,53 @@ function handleLoadPosts() {
   reqComments.addEventListener("load", handleLoadComments);
   reqComments.send();
 
-
   let responseData = JSON.parse(reqPosts.responseText);
+  allPosts = responseData;
   renderPosts(responseData);
 }
 
 const handleLoadComments = () => {
   const commentData = JSON.parse(reqComments.responseText);
-  console.log(commentData);
-  
+  allComments = commentData;
+
+  const createdData = allPosts.map((post) => {
+    const relatedComments = allComments.filter(
+      (comment) => comment.postId === post.id
+    );
+
+    return {
+      userId: post.userId,
+      id: post.id,
+      title: post.title,
+      body: post.body,
+      comments: relatedComments,
+    };
+  });
+  renderPosts(createdData);
 };
 
 function renderComments(comment) {}
 
-
-
 function renderPosts(posts) {
   const template = posts
     .map((post) => {
+      console.log(post.comments);
+
       return `
   <div>
   <h2>${post.title}</h2>
   <p>${post.body}</p>
+  <div class="comments-container">
+  ${post.comments
+    .map((comment) => {
+      return `
+    <div>${comment.body}</div>
+    
+    `;
+    })
+    .join("-")}
+  </div>
+
   </div>
   `;
     })
